e30e8aa7925e8ae3f0d3711ecb35002cfe444dd5
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-06-04 22:17:35 +0300

    update static file settings

--- insta/urls.py
+++ insta/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-  path('',views.index, name='index')
+  path('',views.index, name='index'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- instaclone/urls.py
+++ instaclone/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('insta.urls')),
+    path('', include('insta.urls'))
 ]
