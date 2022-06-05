e1a4bc609e02639e871694a3a493ae94236f964b
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-06-06 00:59:35 +0300

    update templates, userprofile function

--- insta/urls.py
+++ insta/urls.py
@@ -6,8 +6,9 @@ from django.conf import settings
 
 urlpatterns = [
     path('', index, name='index'),
+		path('accounts/profile/', UserProfile, name='profile'),
 		path('signup/',signup,name = 'signup'),
-    path('login/',auth_views.LoginView.as_view(template_name='login.html'),name = 'login'),
+    path('accounts/login/',auth_views.LoginView.as_view(template_name='login.html'),name = 'login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='logout.html'),name = 'logout'),
    	path('newpost/', NewPost, name='newpost'),
    	path('<uuid:post_id>', PostDetails, name='postdetails'),
@@ -17,3 +18,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
--- insta/views.py
+++ insta/views.py
@@ -151,3 +151,41 @@ def like(request, post_id):
 	post.save()
 
 	return HttpResponseRedirect(reverse('postdetails', args=[post_id]))
+
+def UserProfile(request, username):
+	user = get_object_or_404(User, username=username)
+	profile = Profile.objects.get(user=user)
+	url_name = resolve(request.path).url_name
+	
+	if url_name == 'profile':
+		posts = Post.objects.filter(user=user).order_by('-posted')
+
+	else:
+		posts = profile.favorites.all()
+
+	#Profile info box
+	posts_count = Post.objects.filter(user=user).count()
+	following_count = Follow.objects.filter(follower=user).count()
+	followers_count = Follow.objects.filter(following=user).count()
+
+	#follow status
+	follow_status = Follow.objects.filter(following=user, follower=request.user).exists()
+
+	#Pagination
+	paginator = Paginator(posts, 8)
+	page_number = request.GET.get('page')
+	posts_paginator = paginator.get_page(page_number)
+
+	template = loader.get_template('profile.html')
+
+	context = {
+		'posts': posts_paginator,
+		'profile':profile,
+		'following_count':following_count,
+		'followers_count':followers_count,
+		'posts_count':posts_count,
+		'follow_status':follow_status,
+		'url_name':url_name,
+	}
+
+	return HttpResponse(template.render(context, request))
