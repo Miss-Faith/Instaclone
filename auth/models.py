0d08098f6e56698795eebdaf9e1eaeb50f49479c
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-06-05 14:14:48 +0300

    Add auth app, create profile model

---
+++ auth/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+	first_name = models.CharField(max_length=50, null=True, blank=True)
+	last_name = models.CharField(max_length=50, null=True, blank=True)
+	location = models.CharField(max_length=50, null=True, blank=True)
+	url = models.CharField(max_length=80, null=True, blank=True)
+	profile_info = models.TextField(max_length=150, null=True, blank=True)
+	created = models.DateField(auto_now_add=True)
+	favorites = models.ManyToManyField(Post)
+	picture = models.ImageField(upload_to=user_directory_path, blank=True, null=True, verbose_name='Picture')
+
+	def save(self, *args, **kwargs):
+		super().save(*args, **kwargs)
+		SIZE = 250, 250
+
+		if self.picture:
+			pic = Image.open(self.picture.path)
+			pic.thumbnail(SIZE, Image.LANCZOS)
+			pic.save(self.picture.path)
+
+	def __str__(self):
+		return self.user.username
+		
+
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+	instance.profile.save()
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
