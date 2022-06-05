e916695e4d0882bd27c6d573627fb6c6827476de
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-06-05 14:32:56 +0300

    Add update settings and project urls with auth

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'insta',
+    'auth',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- instaclone/urls.py
+++ instaclone/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from authy.views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('insta.urls'))
+    path('', include('insta.urls')),
+    path('user/', include('auth.urls')),
+    path('<username>/', UserProfile, name='profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
