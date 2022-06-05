11e13129ae7878315651409aa2a608e28c38b74d
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-06-05 14:28:04 +0300

    Add edit-profile view and update auth urls

--- auth/urls.py
+++ auth/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as authViews 
 
 urlpatterns = [
-  path()
+  path('profile/edit', EditProfile, name='edit-profile'),
+  path('signup/', Signup, name='signup'),
+  path('login/', authViews.LoginView.as_view(template_name='login.html'), name='login'),
+  path('logout/', authViews.LogoutView.as_view(), {'next_page' : 'index'}, name='logout'),
 ]
