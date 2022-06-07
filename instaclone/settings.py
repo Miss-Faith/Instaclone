c764f9d92ee3610c6efe6a9e3af5f521d4345e93
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-06-07 12:40:16 +0300

    Apply page styles

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -136,3 +136,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = 'login'
